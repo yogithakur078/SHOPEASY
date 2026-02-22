9be7863d37fcd3e467d3e814c410e39e87f2ed7e
yogithakur078 <yogithakur078@gmail.com>
2026-02-22 16:42:20 +0530

    loading option add

--- api.js
+++ api.js
@@ -1,18 +1,25 @@
 const container = document.getElementById("container");
+const loading = document.getElementById("loading");
 
-const products = fetch("https://dummyjson.com/products")
+// show loading
+loading.style.display = "block";
+
+fetch("https://dummyjson.com/products")
   .then(res => res.json())
   .then(data => {
-    let item = []
+
+    loading.style.display = "none"; // hide loading after data comes
+
+    let item = [];
     for (let i = 0; i < 30; i++) {
-      item.push(data.products[i])
+      item.push(data.products[i]);
     }
 
     let total = "";
 
     item.forEach(items => {
       total += `
-        <a  href="product.html?id=${items.id}"  class="product-card">
+        <a href="product.html?id=${items.id}" class="product-card">
           <img src=${items.thumbnail} alt="">
           <p>${items.title}</p>
           <div class="price">₹ ${items.price}</div>
@@ -23,4 +30,7 @@ const products = fetch("https://dummyjson.com/products")
 
     container.innerHTML = total;
   })
-  .catch(err => console.log(err));
+  .catch(err => {
+    loading.innerText = "Failed to load products ❌";
+    console.log(err);
+  });
